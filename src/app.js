2dda0d28e4b82e168ad000270e15ed338efdfc46
Jhoann <jhoannegd@gmail.com>
2023-03-27 19:53:59 +0000

    ajustes finales

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var k = 0; k < noun.length; k++) {
+        var dominios = pronoun[i] + adj[j] + noun[k] + ".com";
+        console.log(dominios);
+      }
+    }
+  }
 };
