8a87a6a1625f70aaf137d4e0af7ece93b3d7fb8a
Jhoann <64670851+Jansooxx@users.noreply.github.com>
2023-03-29 19:50:00 +0200

    ajustes declaracion variables

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  var pronoun = ["the", "our"];
-  var adj = ["great", "big"];
-  var noun = ["jogger", "racoon"];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-  for (var i = 0; i < pronoun.length; i++) {
-    for (var j = 0; j < adj.length; j++) {
-      for (var k = 0; k < noun.length; k++) {
-        var dominios = pronoun[i] + adj[j] + noun[k] + ".com";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let dominios = pronoun[i] + adj[j] + noun[k] + ".com";
         console.log(dominios);
       }
     }
